98d39a2258957f8d9396b95b2a81fa01489de284
Onkar Dherange <51067778+OnkarSD@users.noreply.github.com>
2020-10-23 21:38:32 +0530

    Add files via upload

---
+++ Day 19/leet.py
@@ -0,0 +1,14 @@
+#902. Numbers At Most N Given Digit Set
+
+    def atMostNGivenDigitSet(self, D, N):
+        N = str(N)
+        n = len(N)
+        res = sum(len(D) ** i for i in range(1, n))
+        i = 0
+        while i < len(N):
+            res += sum(c < N[i] for c in D) * (len(D) ** (n - i - 1))
+            if N[i] not in D: break
+            i += 1
+        return res + (i == n)
+
+        
